bd724b21b3f65bbc696474ff0c3a7d6ee44c829d
Wagalidoom <lukas-8@live.fr>
2024-08-30 18:31:22 +0200

    feat: add button to swap selected image

--- components/ImgUpload.tsx
+++ components/ImgUpload.tsx
@@ -1,5 +1,5 @@
-import { ArrowPathIcon, PlusIcon } from "@heroicons/react/24/outline";
-import React, { useState, useEffect } from "react";
+import { ArrowPathRoundedSquareIcon, PlusIcon } from "@heroicons/react/24/outline";
+import React, { useState, useEffect, useRef } from "react";
 import { useDropzone } from "react-dropzone";
 
 type AvatarUploadProps = {
@@ -15,7 +15,7 @@ const AvatarUpload: React.FC<AvatarUploadProps> = ({
   const [preview, setPreview] = useState<string | null>(imgUrl || null);
   const [isDragging, setIsDragging] = useState(false);
 
-  const { getRootProps, getInputProps } = useDropzone({
+  const { getRootProps, getInputProps, open } = useDropzone({
     accept: {
       "image/*": [".jpeg", ".png"],
     },
@@ -52,8 +52,12 @@ const AvatarUpload: React.FC<AvatarUploadProps> = ({
     };
   }, []);
 
+  const handleIconClick = () => {
+    open();
+  };
+
   return (
-    <div className="flex items-center">
+    <div className="flex flex-col items-center">
       <div
         {...getRootProps()}
         className={`w-28 h-28 bg-black/5 my-2 text-black/30 rounded-full cursor-pointer text-center flex items-center justify-center text-xs ${
@@ -62,14 +66,12 @@ const AvatarUpload: React.FC<AvatarUploadProps> = ({
       >
         <input {...getInputProps()} />
         {preview ? (
-          // eslint-disable-next-line @next/next/no-img-element
-          <div className="w-full h-full p-1 rounded-full relative">
+          <div className="w-full h-full p-1 rounded-full">
             <img
               src={preview}
               alt="Avatar preview"
               className="w-full h-full rounded-full object-cover cursor-pointer"
             />
-            <ArrowPathIcon className="w-5 h-5 absolute top-2" />
           </div>
         ) : isDragging ? (
           <p className="text-accent">
@@ -79,10 +81,16 @@ const AvatarUpload: React.FC<AvatarUploadProps> = ({
         ) : (
           <div className="flex flex-col items-center gap-y-2">
             <PlusIcon className="w-7 h-7" />
-            {/* <p className="text-zinc">Drag image to upload or browse</p> */}
           </div>
         )}
       </div>
+      
+      {preview ? (
+        <ArrowPathRoundedSquareIcon 
+          className="w-5 h-5 text-zinc cursor-pointer" 
+          onClick={handleIconClick}
+        />
+      ) : null}
     </div>
   );
 };
