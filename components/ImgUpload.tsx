b90bae6468a993e7e6a158570abf6abbb82469da
Wagalidoom <lukas-8@live.fr>
2024-08-30 14:02:52 +0200

    feat img upload component style wip

--- components/ImgUpload.tsx
+++ components/ImgUpload.tsx
@@ -1,3 +1,4 @@
+import { ArrowPathIcon, PlusIcon } from "@heroicons/react/24/outline";
 import React, { useState, useEffect } from "react";
 import { useDropzone } from "react-dropzone";
 
@@ -55,25 +56,31 @@ const AvatarUpload: React.FC<AvatarUploadProps> = ({
     <div className="flex items-center">
       <div
         {...getRootProps()}
-        className={`w-28 h-28 bg-white my-2 text-black/30 border-2 border-dashed rounded-full cursor-pointer text-center flex items-center justify-center text-xs ${
+        className={`w-28 h-28 bg-black/5 my-2 text-black/30 rounded-full cursor-pointer text-center flex items-center justify-center text-xs ${
           isDragging ? "border-accent" : "border-zinc"
-        }`}
+        } ${preview ? "border-2 border-solid" : "border border-dashed"}`}
       >
         <input {...getInputProps()} />
         {preview ? (
           // eslint-disable-next-line @next/next/no-img-element
-          <img
-            src={preview}
-            alt="Avatar preview"
-            className="w-28 h-28 rounded-full object-cover cursor-pointer"
-          />
+          <div className="w-full h-full p-1 rounded-full relative">
+            <img
+              src={preview}
+              alt="Avatar preview"
+              className="w-full h-full rounded-full object-cover cursor-pointer"
+            />
+            <ArrowPathIcon className="w-5 h-5 absolute top-2" />
+          </div>
         ) : isDragging ? (
           <p className="text-accent">
             Drop the file <br />
             here
           </p>
         ) : (
-          <p className="text-zinc">Drag image to upload or browse</p>
+          <div className="flex flex-col items-center gap-y-2">
+            <PlusIcon className="w-7 h-7" />
+            {/* <p className="text-zinc">Drag image to upload or browse</p> */}
+          </div>
         )}
       </div>
     </div>
