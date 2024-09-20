46a46953ef2717ed57e8c60af75bde050028b723
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2024-09-20 17:03:48 +0200

    show v1 members

--- components/members/ProfilePreview.tsx
+++ components/members/ProfilePreview.tsx
@@ -75,13 +75,15 @@ export default function ProfilePreview({
         />
       ) : (
         <div className='w-[30px] h-[30px] min-w-[30px] min-h-[30px] bg-primary text-white rounded-full flex items-center justify-center text-sm font-semibold'>
-          {profile.name.charAt(0).toUpperCase()}
+          {profile.name ? profile.name.charAt(0).toUpperCase() : ''}
         </div>
       )}
       <div className='flex flex-col flex-1'>
-        <div className='mx-2 font-bold'>{profile.name}</div>
+        <div className='mx-2 font-bold'>
+          {profile.name ? profile.name : profile.address}
+        </div>
         <div className='mx-2 text-xs text-zinc-500 break-all'>
-          {profile.address}
+          {profile.name ? profile.address : 'v1 profile'}
         </div>
       </div>
       <Button
