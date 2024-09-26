8299cea53b432b2f2c130bac10d04654930c0fab
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2024-09-26 22:39:32 +0200

    footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -10,21 +10,29 @@ export default function Footer() {
       <div className='flex flex-col items-center  mt-4 sm:mt-0'>
         <Image src='/circles.svg' alt='Circles Logo' width={105} height={105} />
         <p className='text-[6px] leading-[6px] sm:text-[8px] sm:leading-[8px] mt-2 '>
-          CIRCLES GROUP MANAGEMENT
+          GROUP MANAGEMENT
         </p>
       </div>
-      <div className='flex flex-1'>
-        <Link
+      <div className='flex'>
+        {/* <Link
           target='_blank'
           className='flex flex-1 justify-center text-sm text-centerlg:text-base items-center underline hover:text-white/80'
           href={'https://www.aboutcircles.com/'}
         >
           Learn more about
           <br className='sm:hidden' /> the Circles Project
-        </Link>
-        <div className='flex justify-center items-center'>
+        </Link> */}
+        {/* <div className='flex justify-center items-center'>
           <DropDown />
-        </div>
+        </div> */}
+
+        <Link
+          target='_blank'
+          className='flex flex-1 justify-center text-sm text-centerlg:text-base items-center underline hover:text-white/80'
+          href={'https://docs.aboutcircles.com/'}
+        >
+          Official docs
+        </Link>
       </div>
     </div>
   );
